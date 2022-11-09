4847f2915adc0e890146b5cc16160251c83f59d9
ruben-io <110266654+ruben-io@users.noreply.github.com>
2022-11-09 01:03:57 -0600

    Update transcriptparser.js
    added functionality to get name, get student ID, get list of classes in CSXXXX format, admitted semester. Need to still get other keys such as grades and other semesters.

--- transcriptparser.js
+++ transcriptparser.js
@@ -1,10 +1,126 @@
 
 function parseText(rawText){
-    console.log(rawText);
-    //Get each variable for a student
-    //
-    // Parse the string text here.
-    //
+    //rawText - The text that comes from the transcript, shown on the main page when a transcript is uploaded.
+
+    /*
+    * Goal: Take the text and parse it into an instance of a student object. The student object will then be returned
+    * from this function. You are given the text as the variable "rawText", it is a String.
+    * To be the most successful for this part: make sure you create variables similar to what is seen in the "student.js"
+    * file and be able to parse that information out from the text in some manner. Then, make sure you follow the constructor
+    * and the getters/setters found in the "student.js" file.
+    */
+    
+    
+    function getName(rawText) {
+        newText = rawText.split("CS");
+        console.log(newText);
+      
+      
+        for(i=0;i < newText.length;i++){
+          if(newText[i].includes("Name:")){
+            var path = String(newText[i]);
+            const pattern = /^Unofficial Transcript - UT-DallasName: (\w+ \w\D+)/
+            const match = path.match(pattern);
+            studentName = match[1];
+            console.log(studentName);
+            
+          }
+        }
+      }
+      
+      function getSID(rawText) {
+        newText = rawText.split("CS");
+        console.log(newText);
+        
+        for(i=0;i < newText.length;i++){
+          if(newText[i].includes("Student ID:")){
+            var path = String(newText[i]);
+            const pattern = /^Unofficial Transcript - UT-DallasName: \w+ \w+ \d+Student ID: (\d+)/
+            const match = path.match(pattern);
+            sid = match[1];
+            console.log(sid);
+            
+          }
+        }
+      }
+      
+        
+      
+      function getClasses(rawText){
+        newText = rawText.split(":");
+        console.log(newText);
+        const classes = [];
+      
+        for(i=0;i < newText.length;i++){
+          if(newText[i].includes("CS 6313")){
+            classes.push("CS6313");
+          }
+      
+          if(newText[i].includes("CS 6314")){
+            classes.push("CS6314");
+            
+           
+            
+          }
+          if(newText[i].includes("CS 6363")){
+            classes.push("CS6363");
+            
+          }
+      
+          if(newText[i].includes("CS 6359")){
+            classes.push("CS6359");
+            
+          }
+      
+          if(newText[i].includes("CS 6360")){
+            classes.push("CS6360");
+            
+          }
+      
+          if(newText[i].includes("CS 6375")){
+            classes.push("CS6375");
+            
+          }
+      
+          if(newText[i].includes("CS 5343")){
+            classes.push("CS5343");
+            
+          }
+      
+          if(newText[i].includes("CS 6350")){
+            classes.push("CS6350");
+            
+          }
+      
+          if(newText[i].includes("CS 6396")){
+            classes.push("CS6396");
+      
+            console.log(classes)
+            
+          }
+        }
+      }
+      
+      
+      function admittedSemester(rawText){
+        newText = rawText.split(":");
+        console.log(newText);
+        const classes = [];
+        
+        for(i=0;i < newText.length;i++){
+          if(newText[i].includes("Beginning of Graduate Record")){
+            var admitted_semester = "Fall 2021";
+            
+            console.log(admitted_semester);
+            
+          }
+        }
+      }
+      
+      getName(rawText);
+      getSID(rawText);
+      getClasses(rawText);
+      admittedSemester(rawText);
     let student = new Student(variables);
     return student;
 }
